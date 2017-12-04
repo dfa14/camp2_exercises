6d3050eb678b498f2954a7df7511227a883487ab
Dom Firinga <dominique.firinga@decathlon.com>
2017-12-04 09:10:45 +0100

    Commit du lundi matin

--- 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
+++ 02_programming_fundamentals/06_your_first_program/03_memory/memory.js
@@ -1,2 +1,32 @@
 // This function will clear the terminal when called
 const clear = require("cli-clear");
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const cards = ["🐰", "🐰", "🎃", "🎃", "🌲","🌲"];
+const cardsHidden = Array(6).fill("X");
+
+
+function askTheQuestion(answer) {
+  let temp = 2; // définition du nombre de coups
+  for (let i = 0; i < temp; i++) {
+    reader.question("which cards you want to discover ? from 1 to 6", askTheQuestion);
+    //console.log(choice);
+    console.log("test");
+  }
+  reader.close(); // sortie du jeu
+  return;
+}
+
+
+console.log(cards);
+
+console.log(cardsHidden);
+askTheQuestion(2);
+
+
+
+//

56c885caa96ba1e988f0e37dd46a58c70247e06a
Dom Firinga <dominique.firinga@decathlon.com>
2017-12-06 11:18:01 +0100

    Commit du mercredi

---
+++ 03_backend_developement/03_testing_our_code/01_greeting/greeting.test.js
@@ -0,0 +1,21 @@
+/*function greet(name) {
+  let greetingName = name;
+
+  if (!name) {
+    greetingName = "World";
+  }
+
+  return `Hello ${greetingName.toUpperCase()}!`;
+}
+
+module.exports = greet; */
+
+// test à réaliser
+// input -> toto -- output -> Hello TOTO!
+
+
+test("Return Hello + name + !", () => {
+  expect(result).toBe(true);
+  expect(result).toBe(1);
+  expect(result).toBe("something");
+});
